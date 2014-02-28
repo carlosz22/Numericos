8ad5fbeeb8373af68ca5b866cf8c61028573c34c
carlosz22 <carlosez9@hotmail.com>
2014-02-27 20:51:02 -0500

    Edición menor
    Se creó una matriz para mostrar resultado y algunas variables

--- biseccion.m
+++ biseccion.m
@@ -4,14 +4,16 @@
 ## Distribuido con la licencia Public GNU
 
 ## Nombre de archivo: biseccion
+## Código .m para hallar x que f(x)=0 por el método de bisección
 
 ## Autor: Carlos Zuluaga 
 ## Creado: 2014-02-27
 ## PROCESOS NUMÉRICOS
 ## Universidad EAFIT
 
-function [x] = biseccion (a,b,nmax,delta,tole)
+function [tabla] = biseccion (a,b,nmax,delta,tole)
 
+format long
 
 ## Variables para que ingrese la primera vez
 i=0;
@@ -21,10 +23,15 @@ q=a;
 
 while i<nmax & p>delta & error>tole  %Condiciones para que el programa ingrese en el ciclo
   i=i+1; %Contador número de iteraciones
+  tabla(i,1)=i; %Crea una matriz
   x=(a+b)/2; %Parte el intervalo
+  ## Creando una Tabla (Matriz) que muestra los valores que tenga tabla
+  tabla(i,2)=x; 
   y=funcion(x); %Evalúa la función en x
+  tabla(i,3)=y;
   p=abs(y); %Evalúa el valor absoluto de la función en y
   error=abs(x-q); %Error como la diferencia de valor absoluto entre la x actual y la x anterior
+  tabla(i,4)=error;
   q=x; %Guarda la x anterior
   ## Condicional: Si hay cambio de signo entre a y la mitad del intervalo cambia la b por el valor de x
   if funcion(a)*funcion(x)<0 
